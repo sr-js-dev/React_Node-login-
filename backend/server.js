f479d32cca7327fb08597ea001a92d79a4f762e1
madanodyblum <gorayblum@gmail.com>
2020-05-25 13:38:34 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./DB.js');
+const registrationRoutes = require('./route');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' + err) }
+);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(registrationRoutes);
+
+
+module.exports = app
